a88f2e9be1326c37a32069c190f22dea253f3020
partyparrotgreg <2233348+partyparrotgreg@users.noreply.github.com>
2024-08-01 17:01:54 +0100

    page transition

--- src/app/project/[slug]/template.tsx
+++ src/app/project/[slug]/template.tsx
@@ -1,15 +1,19 @@
 "use client";
 
-import { useEffect } from "react";
+import { LayoutTransition } from "@/components/shared/layout-transition";
 
 export default function ProjectTemplate({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  useEffect(() => {
-    // smooth scroll top
-    window.scrollTo(0, 0);
-  }, []);
-  return <>{children}</>;
+  return (
+    <LayoutTransition
+      initial={{ opacity: 0, y: -100 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0, y: -100 }}
+    >
+      {children}
+    </LayoutTransition>
+  );
 }
